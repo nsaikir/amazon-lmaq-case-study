afed7ef6ed6fdfbdae99c6d2dac63a7d87881d1b
N Sai Kiran <114957935+nsaikir@users.noreply.github.com>
2025-06-10 09:17:45 +0530

    Update etl.py

--- etl.py
+++ etl.py
@@ -2,3 +2,8 @@
 import pandas as pd
 import boto3
 import os
+
+s3 = boto3.client('s3')
+obj = s3.get_object(Bucket='lmaq-sample', Key='navigation.csv')
+df = pd.read_csv(obj['Body'])
+df.dropna().to_csv("data/cleaned_navigation.csv", index=False)
